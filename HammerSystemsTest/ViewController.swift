f62412d5e6c7d372c4c6f1d9864fa80a2b541340
Michelangelo <cawabungadude@ya.ru>
2022-01-24 10:21:44 +0300

    Initial Commit

---
+++ HammerSystemsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HammerSystemsTest
+//
+//  Created by Михаил Кулагин on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
